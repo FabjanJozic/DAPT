9012aedb2219db4e3aaf6e8df0e05752bc3cb992
FabjanJozic <100929336+FabjanJozic@users.noreply.github.com>
2024-11-29 23:52:27 +0100

    gotovo LJ

--- vjezba5/LJ_g.py
+++ vjezba5/LJ_g.py
@@ -1,26 +1,26 @@
 import numpy as np
 
-g = []
-r = []
-
 data_file = 'RDF_rho0.8_T1.0_N108'
 rho = 0.8
 T = 1.0
 N = 108
 
-with open(data_file, 'r') as data:
+with open('C:\\Users\\fabja\\Desktop\\programi\\DAPT\\vjezba5\\'+data_file, 'r') as data:
     lin = data.readlines()
+    r = [0]*len(lin)
+    g = [0]*len(lin)
+    dr = [0.0]
     for l in range(len(lin)):
         rval, gval = lin[l].strip().split()
-        r.append(rval)
-        g.append(gval)
+        r[l] = float(rval)
+        g[l] = float(gval)
+        dr.append(r[l])
     data.close()
 
 integ = 0.0
-dr = r[1]-r[0]
 for i in range(len(r)):
     U_LJ = 4*(r[i]**(-12)-r[i]**(-6))
-    integ += g[i]*U_LJ*(r[i]**2)*dr
+    integ += g[i]*U_LJ*(r[i]**2)*(dr[i+1]-dr[i])
 
 U = 2*np.pi*rho*N*integ+1.5*N*T
 
